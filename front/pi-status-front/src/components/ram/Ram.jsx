6b5a89ab258288b309f7a5b07b9f2e665d9163b0
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-01-25 12:04:54 +0100

    Rework and complete RAM front component

--- front/pi-status-front/src/components/ram/Ram.jsx
+++ front/pi-status-front/src/components/ram/Ram.jsx
@@ -1,48 +1,41 @@
 import './Ram.css'
 import { formatBytes } from "../../utils"
 
-import { useState, useEffect } from 'react'
-
-import {
-    PieChart,
-    Pie,
-    Cell,
-    ResponsiveContainer
-} from "recharts";
+import RamStat from './RamStat'
 
 export default function Ram({ramUsage}) {
-    const [pieData, setPieData] = useState([])
-
-    useEffect(() => {
-        if(Object.keys(ramUsage).length != 0) {
-            setPieData([
-                { value: ramUsage.used },
-                { value: ramUsage.available}
-            ])
-        }
-    }, [ramUsage])
-    
-    return <div class="stats-container ram-container">
-        <ResponsiveContainer>
-            <PieChart>
-                <Pie
-                    data={pieData}
-                    cx="50%"
-                    cy="50%"
-                    startAngle={180}
-                    endAngle={0}
-                    innerRadius="90%"
-                    outerRadius="100%"
-                    fill="#95e6cb"
-                    paddingAngle={1}
-                    animationDuration={100}
-                    dataKey="value"
-                    stroke="#0b0e14"
-                >
-                    <Cell key="cell-used" fill="#95e6cb" />
-                    <Cell key="cell-used" fill="#565b66" />
-                </Pie>
-            </PieChart>
-        </ResponsiveContainer>
+    return <div class="stats-container flex-column align-items-center w-100">
+            <div>
+                {ramUsage.total ? formatBytes(ramUsage.total) : 0}
+            </div>
+            <div></div>
+            <div className="w-100 d-flex flex-column align-items-center">
+                <RamStat
+                    total={ramUsage.total || 0}
+                    stat={ramUsage.used ? ramUsage.used : 0}
+                    label="Used"
+                />
+            </div>
+            <div className="w-100 d-flex flex-column align-items-center">
+                <RamStat
+                    total={ramUsage.total || 0}
+                    stat={ramUsage.available ? ramUsage.available : 0}
+                    label="Available"
+                />
+            </div>
+            <div className="w-100 d-flex flex-column align-items-center">
+                <RamStat
+                    total={ramUsage.total || 0}
+                    stat={ramUsage.free ? ramUsage.free : 0}
+                    label="Free"
+                />
+            </div>
+            <div className="w-100 d-flex flex-column align-items-center">
+                <RamStat
+                    total={ramUsage.total || 0}
+                    stat={ramUsage.cached ? ramUsage.cached : 0}
+                    label="Cached"
+                />
+            </div>
     </div>
 }
---
+++ front/pi-status-front/src/components/ram/RamStat.jsx
@@ -0,0 +1,23 @@
+import './RamStat.css'
+
+import { formatBytes } from "../../utils"
+
+import { useState, useEffect } from 'react'
+
+export default function Ram({total, stat, label}) {
+    const [statPerc, setStatPerc] = useState(0)
+
+    useEffect(() => {
+        if(total != 0) setStatPerc((stat / total) * 100)
+    }, [stat, total])
+    
+    return <div className="d-flex ram-stats-container flex-column w-100">
+        <div className="d-flex justify-content-between ram-text">
+            <div>{label}</div>
+            <div>{stat != 0 ? `${formatBytes(stat)} (${statPerc.toFixed(2)}%)` : "0 (0%)"}</div>
+        </div>
+        <div class="progress w-100">
+            <div class="progress-bar ram-bar" role="progressbar" style={{width: `${statPerc}%`}}></div>
+        </div>
+    </div>
+}
