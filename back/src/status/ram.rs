521e3f63ac39c968762b44221592fdb590d89a97
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-01-31 11:34:55 +0100

    Optimize RAM component data retrieval

--- back/src/status/ram.rs
+++ back/src/status/ram.rs
@@ -2,16 +2,15 @@ use super::StatusFields;
 
 use std::fs;
 
-use regex;
 use serde::Serialize;
 use anyhow::Result;
 
 const PROC_MEMINFO: &str = "/proc/meminfo";
 
-const TOTAL_LABEL: &str = "MemTotal";
-const FREE_LABEL: &str = "MemFree";
-const AVAILABLE_LABEL: &str = "MemAvailable";
-const CACHED_LABEL: &str = "Cached";
+const TOTAL_LABEL: &str = "MemTotal:";
+const FREE_LABEL: &str = "MemFree:";
+const AVAILABLE_LABEL: &str = "MemAvailable:";
+const CACHED_LABEL: &str = "Cached:";
 
 const TOTAL: usize = 0;
 const FREE: usize = 1;
@@ -36,14 +35,13 @@ pub struct Ram {
 
 fn get_ram() -> Result<Ram> {
     let meminfo = fs::read_to_string(PROC_MEMINFO)?;
-    let mem_line_regex = regex::Regex::new(r"(: *| kB)").unwrap();
 
     let mut mem_values: Vec<u64> = vec![];
     for line in meminfo.lines() {
-        let split_line = mem_line_regex.split(line).collect::<Vec<&str>>();
-        
+        let split_line = line.split_whitespace().collect::<Vec<&str>>();
+
         if split_line.len() < 2 || !LABELS.contains(&split_line[0]) {continue}
-        
+
         mem_values.push(split_line[1].parse::<u64>()? * 1024);
     }
 
