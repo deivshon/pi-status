b6a0b39e10af3c3343cff636e15667846c72b382
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-03-30 18:29:57 +0100

    refactor: :lipstick: properly style error box

--- front/src/components/app/ErrorBox.tsx
+++ front/src/components/app/ErrorBox.tsx
@@ -1,11 +1,24 @@
+import classNames from "classnames";
+
 type ErrorBoxProps = {
     error: string;
 };
 
 export const ErrorBox = (props: ErrorBoxProps) => {
+    const len = props.error.length;
+
     return (
-        <div className="flex h-full items-center justify-center text-sm">
-            <div className="no-scrollbar h-[50vh] w-[50vw] items-center justify-center overflow-scroll rounded-xl bg-red-400 px-4 py-2">
+        <div className="flex h-full items-center justify-center">
+            <div
+                className={classNames(
+                    "no-scrollbar max-h-[50vh] max-w-[80vw] overflow-y-scroll break-words rounded-xl bg-red-400 px-4 py-2 md:w-[50vw]",
+                    {
+                        "text-xs md:text-xs": len >= 500,
+                        "text-xs md:text-sm": len >= 300 && len < 500,
+                        "text-sm md:text-base": len < 300,
+                    },
+                )}
+            >
                 <p className="text-center text-red-950">{props.error}</p>
             </div>
         </div>
